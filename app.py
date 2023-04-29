0baf0e2755bac1ee987be7dfd399aba7f2cf2a66
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-04-30 01:55:56 +0700

    new version

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import json
-from trade import create_client, rebalance
+from trade import create_client, rebalance, get_value_symbol, get_symbols_info
 from line_notify import LineNotify
 
 app = Flask(__name__)
@@ -8,16 +8,23 @@ app = Flask(__name__)
 LINE_NOTIFY_TOKEN = "MiGiGvEehvIskk3qJrXLFeQ78Adpf9UvLO2wGLgA9JP"
 line_notify = LineNotify(LINE_NOTIFY_TOKEN)
 
+
+
 customers = [
     {
         "id": 0,
         "binance": {
             "api_key": "GRJaQlXdXyjHzKJv6uyfxIO1z7mTqq4QFzF0yFt4REJS1krN9lN2QipoDjfAg9lE",
-            "secret_key": "J6rVvqYmHcSnQ6feekOxetR7WnCVGbDSt7qwzjJoUyMuBE6gpmb6Gb6Q6E9gfj7v"
+            "secret_key": "J6rVvqYmHcSnQ6feekOxetR7WnCVGbDSt7qwzjJoUyMuBE6gpmb6Gb6Q6E9gfj7v",
+            "testnet": True
         },
         "bitkub": {}
     }
 ]
+
+
+HOSTCLIENT = create_client("GRJaQlXdXyjHzKJv6uyfxIO1z7mTqq4QFzF0yFt4REJS1krN9lN2QipoDjfAg9lE", "J6rVvqYmHcSnQ6feekOxetR7WnCVGbDSt7qwzjJoUyMuBE6gpmb6Gb6Q6E9gfj7v", testnet=True)
+symbol_info = get_symbols_info(HOSTCLIENT)
 
 @app.route('/')
 def main():
@@ -27,31 +34,23 @@ def main():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = json.loads(request.data.decode('utf-8'))
-    
-    print(data)
-    
+
     buy_data = data["action"]
     isBuy = buy_data == "BUY"
-    
     symbol = data["symbol"]
-    amount_coin = float(data["amount_coin"])
-    leverage = int(data["lev"])
-    
-    message = data
-    print(message)
-    line_notify.send(str(message))
     
     for customer in customers:
-        print(customer["id"])
-        client = create_client(customer["binance"]["api_key"], customer["binance"]["secret_key"], testnet=True)
-        # rebalance(
-        #     client,
-        #     buy_data,
-        #     symbol1=data['SYMBOL'],
-        #     percent=float(data['AMOUNT_COIN'])
-        #     )
-
-
+        client = create_client(customer["binance"]["api_key"], customer["binance"]["secret_key"], testnet=customer["binance"]["testnet"])
+        success = rebalance(
+            client,
+            isBuy,
+            symbol1=symbol,
+            percent=0.5
+        )
+        if success:
+            value = get_value_symbol(client, symbol, symbol_info[symbol]["quoteAsset"], symbol_info[symbol]["baseAsset"])
+            message = f"Rebalance {symbol} {buy_data}\nValue: {value}"
+            line_notify.send(message)
 
     return "Webhook is working!!!"
 
--- trade.py
+++ trade.py
@@ -17,7 +17,7 @@ SECRET_KEY = "J6rVvqYmHcSnQ6feekOxetR7WnCVGbDSt7qwzjJoUyMuBE6gpmb6Gb6Q6E9gfj7v"
 
 # Diff at least 12 USDT that can be traded
 MIN_DIFF_VALUE = 12
-class MyCLient:    
+class MyCLient:
     def __init__(self, API_KEY, SECRET_KEY, testnet=False) -> None:
         self.client = Client(API_KEY, SECRET_KEY, testnet=testnet)
     def rebalance(self):
@@ -39,16 +39,13 @@ def rebalance(client,
         percent = 0.5
     
     price_symbol1 = float(client.get_symbol_ticker(symbol=symbol)["price"])
-    print(f"Price {symbol1}/{symbol2} = {price_symbol1} {symbol2}")
     
     balance_symbol1 = float(client.get_asset_balance(asset=symbol1)["free"])
     balance_symbol2 = float(client.get_asset_balance(asset=symbol2)["free"])
-    print(f"Balance {symbol1} = {balance_symbol1} {symbol1}\nBalance {symbol2} = {balance_symbol2} {symbol2}")
     
     value_symbol1 = balance_symbol1 * price_symbol1
     value_symbol2 = balance_symbol2
     value_sum = value_symbol1 + value_symbol2
-    print(f"Value {symbol1} = {value_symbol1} {symbol2}\nValue {symbol2} = {value_symbol2} {symbol2}\nSum = {value_sum} {symbol2}")
     
     if percent is not None:
         target_value_symbol1 = value_sum * percent
@@ -56,32 +53,47 @@ def rebalance(client,
         if target_value_symbol1 is None and target_value_symbol2 is not None:
             target_value_symbol1 = value_sum - target_value_symbol2
     
-    print(f"Percent = {percent}\nTarget Value {symbol1} = {target_value_symbol1} {symbol2}")
-    
     dif_quantity = abs(target_value_symbol1-value_symbol1)/(price_symbol1)
     dif_value = dif_quantity * price_symbol1
     
     if dif_value < MIN_DIFF_VALUE:
         print("Can't trade")
         print(f"Diff Value = {dif_value} {symbol2} < {MIN_DIFF_VALUE} {symbol2}")
-        return
+        return False
+    
+    new_balance_symbol1 = balance_symbol1
+    new_balance_symbol2 = balance_symbol2
     
     if target_value_symbol1 < value_symbol1 and not is_buy:
-        balance_symbol1 -= dif_quantity
-        balance_symbol2 += dif_value
-        print(f"SELL {symbol1} {dif_quantity} [{dif_value} {symbol2}]")
-        #client.create_order(symbol=symbol,side="SELL",type="MARKET",quantity=quantity)
+        new_balance_symbol1 -= dif_quantity
+        new_balance_symbol2 += dif_value
+        # client.create_order(symbol=symbol,side="SELL",type="MARKET",quantity=dif_quantity)
     elif target_value_symbol1 > value_symbol1 and is_buy:
-        balance_symbol1 += dif_quantity
-        balance_symbol2 -= dif_value
-        print(f"BUY {symbol1} {dif_quantity} [{dif_value} {symbol2}]")
-        #client.create_order(symbol=symbol,side="BUY",type="MARKET",quantity=quantity)
+        new_balance_symbol1 += dif_quantity
+        new_balance_symbol2 -= dif_value
+        # client.create_order(symbol=symbol,side="BUY",type="MARKET",quantity=dif_quantity)
+    else:
+        print("Do nothing")
+        return False
     
+    new_value_symbol1 = new_balance_symbol1 * price_symbol1
+    new_value_symbol2 = new_balance_symbol2
+    
+    
+    # region print
+    print(f"Price {symbol1}/{symbol2} = {price_symbol1} {symbol2}")
     print(f"Balance {symbol1} = {balance_symbol1} {symbol1}\nBalance {symbol2} = {balance_symbol2} {symbol2}")
-    value_symbol1 = balance_symbol1 * price_symbol1
-    value_symbol2 = balance_symbol2
-    print(f"Value {symbol1} = {value_symbol1} {symbol2}\nValue {symbol2} = {value_symbol2} {symbol2}")
+    print(f"Value {symbol1} = {value_symbol1} {symbol2}\nValue {symbol2} = {value_symbol2} {symbol2}\nSum = {value_sum} {symbol2}")
+    print(f"Percent = {percent}\nTarget Value {symbol1} = {target_value_symbol1} {symbol2}")
+    if not is_buy:
+        print(f"SELL {symbol1} {dif_quantity} [{dif_value} {symbol2}]")
+    else:
+        print(f"BUY {symbol1} {dif_quantity} [{dif_value} {symbol2}]")
     
+    print(f"Balance {symbol1} = {new_balance_symbol1} {symbol1}\nBalance {symbol2} = {new_balance_symbol2} {symbol2}")
+    print(f"Value {symbol1} = {new_value_symbol1} {symbol2}\nValue {symbol2} = {new_value_symbol2} {symbol2}")
+    # endregion
+    return True
 
 def get_symbols_info(client):
     exchange_info = client.get_exchange_info()
@@ -93,8 +105,16 @@ def get_symbols_info(client):
         }
     return exchange_info2
 
+def get_value_symbol(client, symbol="BTCUSDT", symbol1="BTC", symbol2="USDT"):
+    price_symbol1 = float(client.get_symbol_ticker(symbol=symbol)["price"])
+    balance_symbol1 = float(client.get_asset_balance(asset=symbol1)["free"])
+    balance_symbol2 = float(client.get_asset_balance(asset=symbol2)["free"])
+    value_symbol1 = balance_symbol1 * price_symbol1
+    value_symbol2 = balance_symbol2
+    value_symbol = value_symbol1 + value_symbol2
+    return value_symbol
+
 if __name__ == "__main__":
     client = create_client(API_KEY=API_KEY, SECRET_KEY=SECRET_KEY, testnet=True)
-    pprint(get_symbols_info(client))
-    
-    # rebalance(client, percent=0.1)
+    # pprint(get_symbols_info(client))
+    rebalance(client, False, percent=0.5)
